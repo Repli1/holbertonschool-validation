ff18b9691cef39c39c5d2247768b358cda291fa7
Repli1 <cladescandor42@gmail.com>
2023-05-17 02:23:14 -0700

    fix 3

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-curl -L -o https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 tar -xzf hugo.tar.gz hugo
 sudo mv hugo /usr/local/bin/
 apt-get install -y make
